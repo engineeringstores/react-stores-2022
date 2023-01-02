f6fb03e335c2750e3caa10e9524812feed7ea6b0
Natalie Chan <natapoke2@gmail.com>
2023-01-01 22:34:59 -0500

    adding assets

---
+++ client/src/pages/collections/collections.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageCollections = () => {
+  return (
+    <>
+      <div style={{ backgroundColor: 'blue', width: '50px', height: '50px' }}></div>
+    </>
+  );
+};
+
+export { PageCollections };
---
+++ client/src/pages/shop/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageShop = () => {
+  return (
+    <>
+      <div style={{ backgroundColor: 'red', width: '50px', height: '50px' }}></div>
+    </>
+  );
+};
+
+export { PageShop };
