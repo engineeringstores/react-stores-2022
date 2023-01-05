4bbaf945fe654a82387920910c1c26f98f24fc34
Natalie Chan <natapoke2@gmail.com>
2023-01-04 22:06:52 -0500

    minor css changes

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './navbar.scss';
-import { pages } from '../../util/pages';
+//import { pages } from '../../util/pages';
 
 import HorizontalLogo from '../../assets/logo/es-horizontal-logo.svg';
 import SearchIcon from '../../assets/icons/magnifying-glass-solid.svg';
 
 const Navbar = () => {
+  const pages = ['shop', 'collections', 'consignment', 'about us', 'Search'];
+
   return (
     <>
       <div className="navbar-container">
         <img className="navbar-logo" src={HorizontalLogo} />
 
         <div className="navbar-container-links">
-          {pages.navbar.map((item) => {
-            if (item.label == 'Search') {
+          {pages.map((item) => {
+            if (item == 'Search') {
               return (
                 <div className="navbar-link-container" style={{ marginRight: 0 }}>
                   <img className="navbar-search" src={SearchIcon} />
@@ -23,7 +25,7 @@ const Navbar = () => {
               return (
                 <>
                   <div className="navbar-link-container">
-                    <h3 className="navbar-link-text">{item.label.toUpperCase()}</h3>
+                    <h3 className="navbar-link-text">{item.toUpperCase()}</h3>
                     <div className="navbar-underline"></div>
                   </div>
                 </>
